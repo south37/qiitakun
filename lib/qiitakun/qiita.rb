70dea187ce502397303dd3562f9daab5e63bf14b
Nao Minami <south37777@gmail.com>
2016-05-27 20:32:21 +0900

    Implement items

--- lib/qiitakun/qiita.rb
+++ lib/qiitakun/qiita.rb
@@ -1,9 +1,16 @@
+require "rest-client"
+
 module Qiitakun
   class Qiita
-    def initialize
+    API_ENDPOINT = "https://qiita.com/api/v2/"
+
+    def initialize(qiita_token)
+      @qiita_token = qiita_token
     end
 
     def items
+      json = RestClient.get("#{API_ENDPOINT}/items", "Authorization" => "token #{@qiita_token}", accept: :json).body
+      JSON.parse(json, symbolize_names: true).map { |repository| repository[:title] }
     end
   end
 end
