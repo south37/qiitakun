82e2531be1d52ab19c6f726283cd127bede783aa
Nao Minami <south37777@gmail.com>
2016-05-27 20:48:00 +0900

    Fix Authorization header

--- lib/qiitakun/qiita.rb
+++ lib/qiitakun/qiita.rb
@@ -2,14 +2,14 @@ require "rest-client"
 
 module Qiitakun
   class Qiita
-    API_ENDPOINT = "https://qiita.com/api/v2/"
+    API_ENDPOINT = "https://qiita.com/api/v2"
 
     def initialize(qiita_token)
       @qiita_token = qiita_token
     end
 
     def items
-      json = RestClient.get("#{API_ENDPOINT}/items", "Authorization" => "token #{@qiita_token}", accept: :json).body
+      json = RestClient.get("#{API_ENDPOINT}/items", "Authorization" => "Bearer #{@qiita_token}", accept: :json).body
       JSON.parse(json, symbolize_names: true).map { |repository| repository[:title] }
     end
   end
